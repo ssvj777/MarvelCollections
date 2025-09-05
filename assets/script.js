2613dfb31bda1a48c22b738d49dc4aac960866b8
ssvj-07 <ssvijay2807@gmail.com>
2025-09-05 18:45:25 +0530

    Special Presentations Uploaded...

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,63 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+// Animation script for Marvel Movies
+    const animationMarvelMovies = ["The Infinity Saga"];
+
+  // Typing animation script start
+    new Typed("#marvelMovies", { 
+        strings: animationMarvelMovies,
+        typeSpeed: 47,
+        backSpeed: 222,
+        loop: true,
+        showCursor: false
+    });
+
+    const animationMultiverseSaga = ["The Multiverse Saga"];
+
+  // Typing animation script start
+    new Typed("#multiverse", {  // use id selector
+        strings: animationMultiverseSaga,
+        typeSpeed: 47,
+        backSpeed: 222,
+        loop: true,
+        showCursor: false
+    });
+
+// Animation script for Marvel Series
+    const animationMarvelSeries = ["Marvel Series"];
+
+  // Typing animation script start
+    new Typed("#marvelseries", {  // use id selector
+        strings: animationMarvelSeries,
+        typeSpeed: 47,
+        backSpeed: 222,
+        loop: true,
+        showCursor: false
+    });
+
+
+
+// Animation script for Sony Movies
+    const animationSonyMovies = ["Sony Movies"];
+
+  // Typing animation script start
+    new Typed("#sony", {  // use id selector
+        strings: animationSonyMovies,
+        typeSpeed: 47,
+        backSpeed: 222,
+        loop: true,
+        showCursor: false
+    });
+    
+// Animation script for Fox Movies
+    const animationFoxMovies = ["Fox Movies"];
+
+  // Typing animation script start
+    new Typed("#fox", {  // use id selector
+        strings: animationFoxMovies,
+        typeSpeed: 47,
+        backSpeed: 222,
+        loop: true,
+        showCursor: false
+    });
