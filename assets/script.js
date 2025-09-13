f50ead0931686db93d48cfc0bcc38053c1c6f7ab
ssvj777 <ssvj777@gmail.com>
2025-09-13 21:09:48 +0530

    Close Navbar Updated...

--- assets/script.js
+++ assets/script.js
@@ -1,85 +1,94 @@
-//Navbar Script 
+//Navbar Script
 
-let sideNavbar = document.querySelector(".sideNavbar")
+let sideNavbar = document.querySelector(".sideNavbar");
+let showToggle = document.querySelector(".showNavbar");
+let showCloseBtn = document.querySelector(".closeNavbar");
 
-document.querySelector(".togglebtn").addEventListener("click",function(){
-    sideNavbar.style.left = "0"
-})
+document.querySelector(".togglebtn").addEventListener("click", function () {
+  sideNavbar.style.left = "0";
+  showToggle.style.display = "none";
+  showCloseBtn.style.display = "flex";
+});
 
-document.querySelector(".xMark").addEventListener("click",function(){
-    sideNavbar.style.left = "-70%"
-})
-
+document.querySelector(".xMark").addEventListener("click", function () {
+  sideNavbar.style.left = "-70%";
+  showToggle.style.display = "flex";
+  showCloseBtn.style.display = "none";
+});
 
 // Close the menu when a link is clicked
 
 document.addEventListener("DOMContentLoaded", function () {
-    const listcontents = document.querySelectorAll(".sideNavbar a");
+  const listcontents = document.querySelectorAll(".sideNavbar a");
 
-    listcontents.forEach(function (listcontent) {
-        listcontent.addEventListener("click", function () {
-            document.querySelector(".sideNavbar").style.left = "-70%";
-        });
+  listcontents.forEach(function (listcontent) {
+    listcontent.addEventListener("click", function () {
+      sideNavbar.style.left = "-70%";
+      showToggle.style.display = "flex";
+      showCloseBtn.style.display = "none";
     });
+  });
 });
 
 // Animation script for Marvel Movies
-    const animationMarvelMovies = ["The Infinity Saga"];
+const animationMarvelMovies = ["The Infinity Saga"];
 
-  // Typing animation script start
-    new Typed("#marvelMovies", { 
-        strings: animationMarvelMovies,
-        typeSpeed: 47,
-        backSpeed: 222,
-        loop: true,
-        showCursor: false
-    });
+// Typing animation script start
+new Typed("#marvelMovies", {
+  strings: animationMarvelMovies,
+  typeSpeed: 47,
+  backSpeed: 222,
+  loop: true,
+  showCursor: false,
+});
 
-    const animationMultiverseSaga = ["The Multiverse Saga"];
+const animationMultiverseSaga = ["The Multiverse Saga"];
 
-  // Typing animation script start
-    new Typed("#multiverse", {  // use id selector
-        strings: animationMultiverseSaga,
-        typeSpeed: 47,
-        backSpeed: 222,
-        loop: true,
-        showCursor: false
-    });
+// Typing animation script start
+new Typed("#multiverse", {
+  // use id selector
+  strings: animationMultiverseSaga,
+  typeSpeed: 47,
+  backSpeed: 222,
+  loop: true,
+  showCursor: false,
+});
 
 // Animation script for Marvel Series
-    const animationMarvelSeries = ["Marvel Series"];
+const animationMarvelSeries = ["Marvel Series"];
 
-  // Typing animation script start
-    new Typed("#marvelseries", {  // use id selector
-        strings: animationMarvelSeries,
-        typeSpeed: 47,
-        backSpeed: 222,
-        loop: true,
-        showCursor: false
-    });
-
-
+// Typing animation script start
+new Typed("#marvelseries", {
+  // use id selector
+  strings: animationMarvelSeries,
+  typeSpeed: 47,
+  backSpeed: 222,
+  loop: true,
+  showCursor: false,
+});
 
 // Animation script for Sony Movies
-    const animationSonyMovies = ["Sony Movies"];
+const animationSonyMovies = ["Sony Movies"];
 
-  // Typing animation script start
-    new Typed("#sony", {  // use id selector
-        strings: animationSonyMovies,
-        typeSpeed: 47,
-        backSpeed: 222,
-        loop: true,
-        showCursor: false
-    });
-    
+// Typing animation script start
+new Typed("#sony", {
+  // use id selector
+  strings: animationSonyMovies,
+  typeSpeed: 47,
+  backSpeed: 222,
+  loop: true,
+  showCursor: false,
+});
+
 // Animation script for Fox Movies
-    const animationFoxMovies = ["Fox Movies"];
+const animationFoxMovies = ["Fox Movies"];
 
-  // Typing animation script start
-    new Typed("#fox", {  // use id selector
-        strings: animationFoxMovies,
-        typeSpeed: 47,
-        backSpeed: 222,
-        loop: true,
-        showCursor: false
-    });
+// Typing animation script start
+new Typed("#fox", {
+  // use id selector
+  strings: animationFoxMovies,
+  typeSpeed: 47,
+  backSpeed: 222,
+  loop: true,
+  showCursor: false,
+});
